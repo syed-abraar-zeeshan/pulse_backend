ed316cf16b8716fd2f3a9a31d032c2db862a8598
Syed Abraar Zeeshan <abraar.zeeshan@vrindasoft.com>
2026-07-15 15:34:44 +0530

    Implemented search user, remove friend

--- src/features/friends/friends.controller.js
+++ src/features/friends/friends.controller.js
@@ -54,7 +54,109 @@ const getFriendRequests = async (req, res) => {
   });
 };
 
+const acceptFriendRequest = async (req, res) => {
+  const currentUserId = req.user.userId;
+
+  const senderId = req.params.userId;
+
+  const currentUser = await User.findById(currentUserId);
+  const sender = await User.findById(senderId);
+
+  if (!currentUser || !sender) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  // Remove request
+  currentUser.friendRequests.pull(senderId);
+
+  // Add both users as friends
+  currentUser.friends.push(senderId);
+
+  sender.friends.push(currentUserId);
+
+  // Save changes
+  await currentUser.save();
+
+  await sender.save();
+
+  return res.status(200).json({
+    success: true,
+    message: "Friend request accepted successfully",
+  });
+};
+
+const getFriends = async (req, res) => {
+  const userId = req.user.userId;
+
+  const user = await User.findById(userId).populate(
+    "friends",
+    "name email profilePicture",
+  );
+
+  res.status(200).json({
+    success: true,
+    data: user.friends,
+  });
+};
+
+const removeFriend = async (req, res) => {
+  const currentUserId = req.user.userId;
+  const friendId = req.params.userId;
+
+  const currentUser = await User.findById(currentUserId);
+  const friend = await User.findById(friendId);
+
+  if (!currentUser || !friend) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  currentUser.friends = currentUser.friends.filter(
+    (id) => id.toString() !== friendId,
+  );
+
+  friend.friends = friend.friends.filter(
+    (id) => id.toString() !== currentUserId,
+  );
+
+  await currentUser.save();
+
+  await friend.save();
+
+  return res.status(200).json({
+    success: true,
+    message: "Friend removed successfully",
+  });
+};
+
+const searchUsers = async (req, res) => {
+  const searchText = req.query.name;
+  const currentUserId = req.user.userId;
+
+  const users = await User.find({
+    _id: { $ne: currentUserId },
+    name: {
+      $regex: searchText,
+      $options: "i",
+    },
+  }).select("name email profilePicture");
+
+  return res.status(200).json({
+    success: true,
+    data: users,
+  });
+};
+
 module.exports = {
   sendFriendRequest,
   getFriendRequests,
+  acceptFriendRequest,
+  getFriends,
+  removeFriend,
+  searchUsers,
 };
--- src/features/friends/friends.routes.js
+++ src/features/friends/friends.routes.js
@@ -5,11 +5,19 @@ const authMiddleware = require("../../middlewares/auth_middleware");
 const {
   sendFriendRequest,
   getFriendRequests,
+  acceptFriendRequest,
+  getFriends,
+  removeFriend,
+  searchUsers,
 } = require("./friends.controller");
 
 const router = express.Router();
 
 router.post("/request/:userId", authMiddleware, sendFriendRequest);
 router.get("/requests", authMiddleware, getFriendRequests);
+router.post("/accept/:userId", authMiddleware, acceptFriendRequest);
+router.get("/", authMiddleware, getFriends);
+router.delete("/:userId", authMiddleware, removeFriend);
+router.get("/search", authMiddleware, searchUsers);
 
 module.exports = router;
